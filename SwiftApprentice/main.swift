4d3b8f701cb009c5c3b87e02a01847f1abfb12a1
Wellesley Yu <byu@lcs.on.ca>
2019-11-22 14:32:50 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Yu, Boning on 2019-11-22.
+//  Copyright © 2019 Wellesley Yu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
